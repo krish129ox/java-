4d6d25823b4a2c55562f2a7b93eb05332836bf9f
Krish Gupta <krishrajgupta129@gmail.com>
2026-03-29 16:50:43 +0530

    my odd even code

---
+++ oddeven.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class oddeven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+    
+    System.out.print("enter your number:-");
+    int number= scanner.nextInt();
+    if(number%2==0){
+        System.out.println("the number is even");
+    }
+    else{
+        System.out.println("the number is odd");
+    }
+    scanner.close();
+
+
+
+    }
+
+
+    } 
+

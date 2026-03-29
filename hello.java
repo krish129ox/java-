9184bd3fb6c181c703eb85b4535f56135a4ef1cf
Krish Gupta <your-email@example.com>
2026-03-29 10:09:14 +0530

    odd even

--- hello.java
+++ hello.java
@@ -6,28 +6,22 @@ public class hello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("enter your marks: ");
-        int marks = scanner.nextInt();
-        if (marks>=90){
-        System.out.println("nigga you on top"); 
+        System.out.print("enter your number:- ");
+        int number = scanner.nextInt();
+        if (number %2==0){
+        System.out.println("the number is even"); 
         }
-        if (marks<90 && marks>=80){
-            System.out.println("dang thats goood");
-        }
-        if(marks<80 && marks>=70){
-            System.out.println("its fine");
-        }
-        if(marks<70 && marks>=60){
-            System.out.println("thats not bad");
-        }
-        else 
+        else
         {
-            System.out.println("nahh bruhh you failed");
+            System.out.println("the number is odd");
         }
         scanner.close();
+        
+        }
+        
     }
             
-        }
+        
         
     
     

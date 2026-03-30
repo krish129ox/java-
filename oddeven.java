bec4cc2f49644741866993705ab1ef58272cab39
Krish Gupta <krishrajgupta129@gmail.com>
2026-03-30 18:02:50 +0530

    grade calculator

--- oddeven.java
+++ oddeven.java
@@ -5,13 +5,22 @@ public class oddeven {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
     
-    System.out.print("enter your number:-");
-    int number= scanner.nextInt();
-    if(number%2==0){
-        System.out.println("the number is even");
+    System.out.print("enter your marks:-");
+    int marks= scanner.nextInt();
+    if(marks>=90){
+        System.out.println("you on top");
+    }
+    else if(marks<90 && marks>=80){
+        System.out.println("its good its cool");
+    }
+    else if(marks<80 && marks>=70){
+        System.out.println("its fine ");
+    }
+    else if(marks<70 && marks>=60){
+        System.out.println("its not bad but still needs improvement");
     }
     else{
-        System.out.println("the number is odd");
+        System.out.println("nigga you fuked up");
     }
     scanner.close();
 

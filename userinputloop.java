b06e79b61e854d1fda8f8019a4c45056814eaf6e
Krish Gupta <krishrajgupta129@gmail.com>
2026-04-03 22:21:37 +0530

    loopsum

--- userinputloop.java
+++ userinputloop.java
@@ -6,9 +6,11 @@ public class userinputloop {
         Scanner scanner =  new Scanner(System.in);
         System.out.print("enter your n:-");
         int n=scanner.nextInt();
+        int sum=0;
         for (int i=0; i<=n; i++)  {
-            System.out.println(i);
+            sum += i;
         }
+        System.out.println("sum of 1 to "+n+"=" +sum);
         scanner.close();
 
     }

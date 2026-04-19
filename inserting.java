db503b454c3ff92bf350b2c489adc738ef7cea81
Krish Gupta <krishrajgupta129@gmail.com>
2026-04-19 13:43:04 +0530

    inserting in java

---
+++ inserting.java
@@ -0,0 +1,23 @@
+public class inserting {
+    public static void main(String[] args) {
+        
+        String name = "patrick";{
+            System.out.printf("hello! i am %s\n",name);
+        }
+        char firstletter ='p';{
+            System.out.printf("first letter of my name is %c\n",firstletter);
+        }
+        double height =173.2;{
+            System.out.printf("my height is %.2f cm\n",height);
+        }
+        int age = 19;{
+            System.out.printf("i am %d years old\n",age);
+        }
+        boolean isstudent =true;{
+            System.out.printf("student: %b\n",isstudent);
+        }
+    }
+
+
+    
+}

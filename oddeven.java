e2151c3ab20de524bf3788e503b0019a73c5169d
krish Gupta <krishrajgupta129@gmail.com>
2026-07-27 15:41:48 +0530

    oddeven

--- oddeven.java
+++ oddeven.java
@@ -3,31 +3,16 @@ import java.util.Scanner;
 
 public class oddeven {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-    
-    System.out.print("enter your marks:-");
-    int marks= scanner.nextInt();
-    if(marks>=90){
-        System.out.println("you on top");
-    }
-    else if(marks<90 && marks>=80){
-        System.out.println("its good its cool");
-    }
-    else if(marks<80 && marks>=70){
-        System.out.println("its fine ");
-    }
-    else if(marks<70 && marks>=60){
-        System.out.println("its not bad but still needs improvement");
-    }
-    else{
-        System.out.println("nigga you fuked up");
-    }
-    scanner.close();
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        n = scanner.nextInt();
 
-
-
+        if (n % 2 == 0) {
+            System.out.println(n + " is even.");
+        } else {
+            System.out.println(n + " is odd.");
+        }
+        scanner.close();
     }
-
-
-    } 
-
+}

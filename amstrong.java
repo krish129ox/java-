e70a6ea75b5d57b9ff413b32c9bfef2dbe9c61d8
krish Gupta <krishrajgupta129@gmail.com>
2026-07-28 13:20:16 +0530

    amstrong

---
+++ amstrong.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class amstrong {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check: ");
+        int number = scanner.nextInt();
+        
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+        
+        scanner.close();
+    }
+
+    public static boolean isArmstrong(int number) { 
+        int originalNumber = number;
+        int sum = 0;
+        int digits = String.valueOf(number).length();
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, digits);
+            number /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+}
